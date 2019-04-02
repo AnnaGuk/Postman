0220eb353752b316d4ebbcd4449d70e25277fe0e
Anna Guk <guk.anna1989@gmail.com>
2019-04-02 22:35:13 +0200

    styles added

--- scripts/helpers.mjs
+++ scripts/helpers.mjs
@@ -1,171 +1,199 @@
-import {getMethod, postMethod, putMethod, patchMethod, deleteMethod} from "./methods.mjs";
+import {
+  getMethod,
+  postMethod,
+  putMethod,
+  patchMethod,
+  deleteMethod
+} from "./methods.mjs";
 
 const createHtmlTag = config => {
-   const tagName = config.tagName !== undefined ? config.tagName: "div";
-   const tag = document.createElement(tagName);
+  const tagName = config.tagName !== undefined ? config.tagName : "div";
+  const tag = document.createElement(tagName);
 
-   if(config.text !== undefined){
-       const text = document.createTextNode(config.text);
-       tag.appendChild(text);
-   }
+  if (config.text !== undefined) {
+    const text = document.createTextNode(config.text);
+    tag.appendChild(text);
+  }
 
-   if(config.classNames !== undefined){
-       config.classNames.forEach(className => tag.classList.add(className));
-   }
+  if (config.classNames !== undefined) {
+    config.classNames.forEach(className => tag.classList.add(className));
+  }
 
-   if(config.event !== undefined){
-       tag.addEventListener(config.event.type, config.event.cb);
-   }
+  if (config.event !== undefined) {
+    tag.addEventListener(config.event.type, config.event.cb);
+  }
 
-   if(config.id !== undefined){
-       tag.id = config.id;
-   }
+  if (config.id !== undefined) {
+    tag.id = config.id;
+  }
 
-   return tag;
+  return tag;
 };
 
 const showResults = (results, responseBoxTag) => {
-    console.log(results)
-    responseBoxTag.innerHTML = JSON.stringify(results, null, 2)
+  console.log(results);
+  responseBoxTag.innerHTML = JSON.stringify(results, null, 2);
 };
 
 const handleMethodsChange = (bodyRows, methodSelectTag) => {
+  if (methodSelectTag.value === "get" || methodSelectTag.value === "delete") {
+    bodyRows.classList.add("hide");
+  } else {
+    bodyRows.classList.remove("hide");
+  }
+};
 
-    if (methodSelectTag.value === "get" || methodSelectTag.value === "delete") {
-        bodyRows.classList.add("hide");
-    }else{
-        bodyRows.classList.remove("hide")
+const handleButtonRow = event => {
+  const row = event.target.parentElement;
+  addRow(row);
+
+  event.target.removeEventListener("click", handleButtonRow);
+  event.target.addEventListener("click", () => {
+    removeRow(row);
+  });
+  event.target.innerText = "Remove line";
+};
+
+const removeRow = row => {
+  row.remove();
+};
+const addRow = row => {
+  row.parentElement.appendChild(createBodyRowHtml());
+};
+
+const serializeData = bodyRows => {
+  const inputsArr = [...bodyRows.querySelectorAll("input")];
+  const inputsVal = inputsArr.map(input => input.value);
+
+  const data = {};
+
+  inputsVal.forEach((input, idx, arr) => {
+    if (idx % 2 === 0) {
+      data[input] = arr[idx + 1];
     }
-};
+  });
 
-const handleButtonRow = (event) => {
-    const row = event.target.parentElement;
-    addRow(row);
-
-    event.target.removeEventListener("click", handleButtonRow);
-    event.target.addEventListener("click", () => {
-        removeRow(row);
-    })
-    event.target.innerText = "Remove line";
-};
-
-const removeRow = row => {row.remove()};
-const addRow = row => {row.parentElement.appendChild(createBodyRowHtml())};
-
-
-const serializeData = (bodyRows) => {
-    const inputsArr = [...bodyRows.querySelectorAll("input")];
-    const inputsVal = inputsArr.map(input => input.value);
-
-    const data = {};
-
-    inputsVal.forEach((input, idx, arr) => {
-        if(idx % 2 === 0){
-            data[input] = arr[idx + 1];
-        }
-    });
-
-    return JSON.stringify(data);
+  return JSON.stringify(data);
 };
 
 const sendRequest = (methodSelectTag, inputTag, responseBoxTag, bodyRows) => {
+  if (inputTag.value === "") {
+    return false;
+  }
 
-    if(inputTag.value === ""){ return false; }
-
-    switch (methodSelectTag.value) {
-        case "get":
-            getMethod(inputTag.value)
-                .then(response => showResults(response, responseBoxTag));
-            break;
-        case "post":
-            const dataPost = serializeData(bodyRows);
-            postMethod(inputTag.value, dataPost)
-                .then(response => showResults(response, responseBoxTag));
-            break;
-        case "put":
-            const dataPut = serializeData(bodyRows);
-            putMethod(inputTag.value, dataPut)
-                .then(response => showResults(response, responseBoxTag));
-            break;
-        case "patch":
-            const dataPatch = serializeData(bodyRows);
-            patchMethod(inputTag.value, dataPatch)
-                .then(response => showResults(response, responseBoxTag));
-            break;
-        case "delete":
-            deleteMethod(inputTag.value)
-                .then(response => showResults(response, responseBoxTag));
-            break;
-
-    }
-
+  switch (methodSelectTag.value) {
+    case "get":
+      getMethod(inputTag.value).then(response =>
+        showResults(response, responseBoxTag)
+      );
+      break;
+    case "post":
+      const dataPost = serializeData(bodyRows);
+      postMethod(inputTag.value, dataPost).then(response =>
+        showResults(response, responseBoxTag)
+      );
+      break;
+    case "put":
+      const dataPut = serializeData(bodyRows);
+      putMethod(inputTag.value, dataPut).then(response =>
+        showResults(response, responseBoxTag)
+      );
+      break;
+    case "patch":
+      const dataPatch = serializeData(bodyRows);
+      patchMethod(inputTag.value, dataPatch).then(response =>
+        showResults(response, responseBoxTag)
+      );
+      break;
+    case "delete":
+      deleteMethod(inputTag.value).then(response =>
+        showResults(response, responseBoxTag)
+      );
+      break;
+  }
 };
-
 
 const createBodyRowHtml = () => {
-    const rowTag = createHtmlTag({});
-    const keyInputTag = createHtmlTag({tagName: "input"});
-    const valueInputTag = createHtmlTag({tagName: "input"});
+  const rowTag = createHtmlTag({ classNames: ["post-container"] });
+  const keyInputTag = createHtmlTag({ tagName: "input", classNames: ["post"] });
+  const valueInputTag = createHtmlTag({
+    tagName: "input",
+    classNames: ["post"]
+  });
 
-    const button = createHtmlTag({
-        tagName: "button",
-        text: "Add next row",
-        event: {
-            type: "click",
-            cb: handleButtonRow
-        }
-    });
+  const button = createHtmlTag({
+    tagName: "button",
+    classNames: ["btn-helper"],
+    text: "Add next row",
+    event: {
+      type: "click",
+      cb: handleButtonRow
+    }
+  });
 
-    rowTag.appendChild(keyInputTag);
-    rowTag.appendChild(valueInputTag);
-    rowTag.appendChild(button);
+  rowTag.appendChild(keyInputTag);
+  rowTag.appendChild(valueInputTag);
+  rowTag.appendChild(button);
 
-    return rowTag;
+  return rowTag;
 };
 
+export const createPostmanAppGui = config => {
+  const wrapperTag = createHtmlTag({ tagName: "main" });
 
-
-export const createPostmanAppGui = (config) => {
-    const wrapperTag = createHtmlTag({tagName: "main"});
-
-    const bodyRows = createHtmlTag({classNames: ["hide"]});
-    const formTag = createHtmlTag({tagName: "section", classNames: ["box-form"]});
-    const methodSelectTag = createHtmlTag({
-        tagName: "select",
-        event: {type: 'change', cb: () => {handleMethodsChange(bodyRows, methodSelectTag)}}
+  const bodyRows = createHtmlTag({ classNames: ["hide"] });
+  const formTag = createHtmlTag({
+    tagName: "section",
+    classNames: ["box-form"]
+  });
+  const methodSelectTag = createHtmlTag({
+    tagName: "select",
+    classNames: ["user-select"],
+    event: {
+      type: "change",
+      cb: () => {
+        handleMethodsChange(bodyRows, methodSelectTag);
+      }
+    }
+  });
+  config.methods.forEach(method => {
+    const methodTag = createHtmlTag({
+      tagName: "option",
+      classNames: ["user-option"],
+      text: method,
+      value: method
     });
-    config.methods.forEach(method => {
-        const methodTag = createHtmlTag({tagName: "option", text: method, value: method});
-        methodSelectTag.appendChild(methodTag);
-    });
-    formTag.appendChild(methodSelectTag);
+    methodSelectTag.appendChild(methodTag);
+  });
+  formTag.appendChild(methodSelectTag);
 
-    const inputTag = createHtmlTag({tagName: "input", id: "source"});
-    formTag.appendChild(inputTag);
+  const inputTag = createHtmlTag({ tagName: "input", id: "source" });
+  formTag.appendChild(inputTag);
 
-    const responseBoxTag = createHtmlTag({tagName: "pre", classNames: ["box-response"]});
+  const responseBoxTag = createHtmlTag({
+    tagName: "pre",
+    classNames: ["box-response"]
+  });
 
-    const buttonSendTag = createHtmlTag({
-        tagName: "button",
-        text: "Send Request",
-        event: {
-            type: 'click',
-            cb: () => {
-                sendRequest(methodSelectTag, inputTag, responseBoxTag, bodyRows);
-            }
-        }
-    });
-    formTag.appendChild(buttonSendTag);
-    wrapperTag.appendChild(formTag);
+  const buttonSendTag = createHtmlTag({
+    tagName: "button",
+    classNames: ["btn-send"],
+    text: "Send Request",
+    event: {
+      type: "click",
+      cb: () => {
+        sendRequest(methodSelectTag, inputTag, responseBoxTag, bodyRows);
+      }
+    }
+  });
+  formTag.appendChild(buttonSendTag);
+  wrapperTag.appendChild(formTag);
 
+  const rowTag = createBodyRowHtml({});
+  bodyRows.appendChild(rowTag);
+  wrapperTag.appendChild(bodyRows);
 
-    const rowTag = createBodyRowHtml({});
-    bodyRows.appendChild(rowTag);
-    wrapperTag.appendChild(bodyRows);
+  wrapperTag.appendChild(responseBoxTag);
 
-
-    wrapperTag.appendChild(responseBoxTag);
-
-    return wrapperTag;
+  return wrapperTag;
 };
-
--- scripts/methods.mjs
+++ scripts/methods.mjs
@@ -1,26 +1,38 @@
-export const getMethod = async (url) => {
-    return await fetch(url, {method: "GET", headers: {"Content-Type": "application/json"}})
-        .then(response => response.json());
+export const getMethod = async url => {
+  return await fetch(url, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" }
+  }).then(response => response.json());
 };
 
 export const postMethod = async (url, body) => {
-    console.log(body);
-    return await fetch(url, {method: "POST", headers: {"Content-Type": "application/json"}, body})
-        .then(response => response.json());
+  console.log(body);
+  return await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body
+  }).then(response => response.json());
 };
 
 export const putMethod = async (url, body) => {
-    return await fetch(url, {method: "PUT", headers: {"Content-Type": "application/json"}, body})
-        .then(response => response.json());
+  return await fetch(url, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body
+  }).then(response => response.json());
 };
 
 export const patchMethod = async (url, body) => {
-    return await fetch(url, {method: "PATCH", headers: {"Content-Type": "application/json"}, body})
-        .then(response => response.json());
+  return await fetch(url, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body
+  }).then(response => response.json());
 };
 
-export const deleteMethod = async (url) => {
-    return await fetch(url,  {method: "DELETE", headers: {"Content-Type": "application/json"}})
-        .then(response => response.json());
+export const deleteMethod = async url => {
+  return await fetch(url, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" }
+  }).then(response => response.json());
 };
-
